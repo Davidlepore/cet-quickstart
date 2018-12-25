6b754990d2aeb96df59265298fa918ec3d782a02
Alex Torres <alextorressk@gmail.com>
2018-12-25 14:27:51 -0500

    Update packages

--- renderer.js
+++ renderer.js
@@ -1,5 +1,45 @@
-const customTitlebar = require('custom-electron-titlebar');
+const { Menu, MenuItem } = require('electron').remote;
+const { Titlebar } = require('custom-electron-titlebar');
 
-new customTitlebar.TitleBar('green', {
-    menu : null
+const titlebar = new Titlebar('green', {
+  minimizable: false
 });
+
+const menu = new Menu();
+menu.append(new MenuItem({
+	label: 'Item 1',
+	submenu: [
+		{
+			label: 'Subitem 1',
+			click() { console.log('Click on subitem 1') }
+		},
+		{
+			type: 'separator'
+		}
+	]
+}));
+
+menu.append(new MenuItem({
+	label: 'Item 2',
+	submenu: [
+		{
+			label: 'Subitem checkbox',
+			type: 'checkbox',
+			checked: true
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Subitem with submenu',
+			submenu: [
+				{
+					label: 'Submenu i&tem 1',
+					accelerator: 'Ctrl+T'
+				}
+			]
+		}
+	]
+}));
+
+titlebar.setMenu(menu);
